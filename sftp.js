38e62477e6dc72069cbf962db53da86638795669
gitter-monotype <ilan.gitter@olapic.com>
2017-08-14 15:26:04 -0400

    change tabs back to 2 spaces

--- sftp.js
+++ sftp.js
@@ -1,80 +1,80 @@
 var inherits = require('util').inherits
-    , EventEmitter = require('events').EventEmitter
-    , Client = require('ssh2').Client
-    , SFTPWrapper = require('./lib/sftp-wrapper') //needed to modify the prototype of ssh2/SFTPWrapper
-    ;
+  , EventEmitter = require('events').EventEmitter
+  , Client = require('ssh2').Client
+  , SFTPWrapper = require('./lib/sftp-wrapper') //needed to modify the prototype of ssh2/SFTPWrapper
+;
 
 module.exports = SFTPClient
 
 function SFTPClient (options) {
-    if (!(this instanceof SFTPClient)) {
-        return new SFTPClient(options);
+  if (!(this instanceof SFTPClient)) {
+    return new SFTPClient(options);
+  }
+
+  var self = this;
+
+  self.closed = true;
+
+  EventEmitter.call(self);
+
+  self.conn = new Client();
+
+  self.conn.on('error', function (err) {
+    //ignore errors if the connection has been closed
+    //this is to prevent a common ECONNRESET error that
+    //occurs.
+    if (self.closed) {
+      return;
     }
 
-    var self = this;
+    self.emit('error', err);
+  });
 
-    self.closed = true;
+  self.conn.on('ready', function () {
+    self.closed = false;
 
-    EventEmitter.call(self);
+    self.conn.sftp(function (err, sftp) {
+      if (err) {
+        self.emit('error', err);
+      }
 
-    self.conn = new Client();
+      var noCopy = ['on', 'once', 'emit', 'end']
 
-    self.conn.on('error', function (err) {
-        //ignore errors if the connection has been closed
-        //this is to prevent a common ECONNRESET error that
-        //occurs.
-        if (self.closed) {
-            return;
+      Object.keys(sftp.constructor.prototype).forEach(function (key) {
+        if (~noCopy.indexOf(key)) {
+          return;
         }
 
-        self.emit('error', err);
+        var val = sftp[key];
+
+        if (typeof val === 'function') {
+          self[key] = sftp[key].bind(sftp);
+        }
+        else {
+          self[key] = sftp[key];
+        }
+      });
+
+      self.emit('ready');
     });
-
-    self.conn.on('ready', function () {
-        self.closed = false;
-
-        self.conn.sftp(function (err, sftp) {
-            if (err) {
-                self.emit('error', err);
-            }
-
-            var noCopy = ['on', 'once', 'emit', 'end']
-
-            Object.keys(sftp.constructor.prototype).forEach(function (key) {
-                if (~noCopy.indexOf(key)) {
-                    return;
-                }
-
-                var val = sftp[key];
-
-                if (typeof val === 'function') {
-                    self[key] = sftp[key].bind(sftp);
-                }
-                else {
-                    self[key] = sftp[key];
-                }
-            });
-
-            self.emit('ready');
-        });
-    });
+  });
 }
 
 inherits(SFTPClient, EventEmitter);
 
 SFTPClient.prototype.connect = function (options) {
-    var self = this;
+  var self = this;
 
-    self.conn.connect.apply(self.conn, arguments);
+  self.conn.connect.apply(self.conn, arguments);
 
-    return self;
+  return self;
 };
 
 SFTPClient.prototype.end = function () {
-    var self = this;
+  var self = this;
 
-    self.conn.end.apply(self.conn, arguments);
-    self.closed = true;
+  self.conn.end.apply(self.conn, arguments);
+  self.closed = true;
 
-    return self;
+  return self;
 };
